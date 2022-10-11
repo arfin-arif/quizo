e2dedc8f0b6e86d377134de575d607bf23a7c6a1
arfin-arif <arifmtbd@gmail.com>
2022-10-11 12:43:17 +0600

    navbar done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+function Header() {
+    return (
+        <>
+            {['md'].map((expand) => (
+                <Navbar key={expand} bg="secondary" expand={expand} className="mb-3">
+                    <Container fluid>
+                        <Navbar.Brand bg="success" className='text-info  h1' href="#">QUIZO</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3 nav-link">
+                                    <Link to='/'>Home</Link>
+                                    <Link to='/statistic'>Statistic</Link>
+                                    <Link to='/blog'>Blog</Link>
+
+
+                                </Nav>
+                                {/* <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-success">Search</Button>
+                                </Form> */}
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    );
+}
+
+export default Header;
